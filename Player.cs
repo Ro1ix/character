502e21e2246dbd15e77702493fac79d90aada48b
Vasiliy <kimmiez@mail.ru>
2023-11-22 16:43:58 +0300

    v0.2

--- Player.cs
+++ Player.cs
@@ -9,64 +9,199 @@ namespace character
     class Player
     {
         private string name;
-        private int x;
-        private int y;
         private bool lager;
-        private int hp;
-        public void Info(int xMax, int yMax)
+        private int hp, hpMax;
+        private int x, y;
+        public void Start()
         {
-            Console.WriteLine("Введите имя персонажа:");
-            name = Console.ReadLine();
-            //Рандомятся координаты
-            Random rand = new Random();
-            x = rand.Next(1, xMax + 1);
-            y = rand.Next(1, yMax + 1);
-            //Цикл для того, чтобы при неправильном вводе лагеря программа не прекращалась ошибкой
-            string input;
+            InfoIn();
+            Console.Clear();
+            InfoOut();
+        }
+        private void InfoIn()
+        {
+            string input = "";    /*Переменная для ввода*/
+            bool error = false;   /*Переменная для прекращения циклов с ошибкой*/
             do
             {
-                Console.WriteLine("\nВыберите лагерь:\n1. Синий      2. Красный");   //Синий - false, красный - true
-                input = Console.ReadLine();
-                switch (input)
+                Console.Write("Введите имя персонажа: ");
+                name = Console.ReadLine();
+                for (int i = 0; i < name.Length; i++)
                 {
-                    case "1":
-                        lager = false;
+                    if (name[i] == ' ' || name == "")
+                    {
+                        error = true;
+                        Console.Write("\nОШИБКА!!! Имя не должно содержать пробелов или пустоту\nНажмите Enter и попробуйте ещё раз . . . ");
+                        do
+                        {
+                            //Nothing
+                        } while (Console.ReadKey(true).Key != ConsoleKey.Enter);
+                        Console.Clear();
                         break;
-                    case "2":
-                        lager = true;
-                        break;
-                    default:
-                        Console.WriteLine("ОШИБКА!!! Попробуйте ещё раз");
-                        break;
+                    }
+                    else
+                        error = false;
                 }
-            } while (input != "1" && input != "2");
-            //
-            hp = 20;
+            } while (error == true);    /*Ввод имени персонажа*/
+            do
+            {
+                Console.WriteLine("\nВыберите лагерь:\n1. Синий      2. Красный");   /*Синий - false, красный - true*/
+                input = Console.ReadLine();
+                if (input == "1")
+                    lager = false;
+                else if (input == "2")
+                    lager = true;
+                else
+                {
+                    Console.Write("\nОШИБКА!!! Нажмите Enter и попробуйте ещё раз . . . ");
+                    do
+                    {
+                        //Nothing
+                    } while (Console.ReadKey(true).Key != ConsoleKey.Enter);
+                    Console.Clear();
+                    Console.WriteLine($"Введите имя персонажа: {name}");
+                }
+            } while (input != "1" && input != "2");    /*Выбор лагеря*/
+            do
+            {
+                Console.Write("\nВведите кол-во здоровья (от 20 до 50): ");
+                input = Console.ReadLine();
+                if (Int32.TryParse(input, out hpMax) == false)
+                {
+                    Console.Write("\nОШИБКА!!! Нажмите Enter и попробуйте ещё раз . . . ");
+                    do
+                    {
+                        //Nothing
+                    } while (Console.ReadKey(true).Key != ConsoleKey.Enter);
+                    Console.Clear();
+                    Console.WriteLine($"Введите имя персонажа: {name}");
+                    Console.WriteLine("\nВыберите лагерь:\n1. Синий      2. Красный");
+                    if (lager == false)
+                        Console.WriteLine(1);
+                    else
+                        Console.WriteLine(2);
+                }
+                else
+                    hp = hpMax;
+            } while (Int32.TryParse(input, out hpMax) == false);    /*Ввод HP персонажа*/
+            do
+            {
+                do
+                {
+                    Console.Write("Введите координаты (от 0 до 10)\nX: ");
+                    input = Console.ReadLine();
+                    if (Int32.TryParse(input, out x) == false)
+                    {
+                        Console.Write("\nОШИБКА!!! Нажмите Enter и попробуйте ещё раз . . . ");
+                        do
+                        {
+                            //Nothing
+                        } while (Console.ReadKey(true).Key != ConsoleKey.Enter);
+                        Console.Clear();
+                        Console.WriteLine($"Введите имя персонажа: {name}");
+                        Console.WriteLine("\nВыберите лагерь:\n1. Синий      2. Красный");
+                        if (lager == false)
+                            Console.WriteLine(1);
+                        else
+                            Console.WriteLine(2);
+                        Console.Write($"\nВведите кол-во здоровья (от 20 до 50): {hp}");
+                    }
+                } while (Int32.TryParse(input, out x) == false);
+                Console.Write("Y: ");
+                input = Console.ReadLine();
+                if (Int32.TryParse(input, out y) == false)
+                {
+                    Console.Write("\nОШИБКА!!! Нажмите Enter и попробуйте ещё раз . . . ");
+                    do
+                    {
+                        //Nothing
+                    } while (Console.ReadKey(true).Key != ConsoleKey.Enter);
+                    Console.Clear();
+                    Console.WriteLine($"Введите имя персонажа: {name}");
+                    Console.WriteLine("\nВыберите лагерь:\n1. Синий      2. Красный");
+                    if (lager == false)
+                        Console.WriteLine(1);
+                    else
+                        Console.WriteLine(2);
+                    Console.Write($"\nВведите кол-во здоровья (от 20 до 50): {hp}");
+                    Console.Write($"Введите координаты (от 0 до 10)\nX: {x}");
+                }
+            } while (Int32.TryParse(input, out y) == false);    /*Ввод координат*/
         }
-        public void Output()
+        public void InfoOut()
         {
             Console.WriteLine($"Имя: {name}");
-            Console.WriteLine($"Координаты: {x}, {y}");
             if (lager == false)
                 Console.WriteLine("Лагерь: Синий");
             else
-                Console.WriteLine("Имя: Красный");
+                Console.WriteLine("Лагерь: Красный");
             Console.WriteLine($"Жизни: {hp}");
+            Console.WriteLine($"Координаты: {x}; {y}");
         }
-        public void Heal()
+        private void moveX()
+        {
+            string input;
+            int move = 0;
+            Console.WriteLine("\n(Положительное число ― вправо,  отрицательное - влево)\nВведите, какое расстояние хотите пройти: ");
+            input = Console.ReadLine();
+            if (Int32.TryParse(input, out move) == true)
+            {
+                if ((x += move) <= 10 || (x+= move) >= 0)
+                {
+                    x += move;
+                    Console.WriteLine($"\nГотово! Ваши текущие координаты: {x}; {y}");
+                }
+                else
+                {
+                    Console.Write("\nОШИБКА!!! Нельзя выходить за пределы карты\nПопробуйте ещё раз . . . ");
+                    moveX();
+                }
+            }
+            else
+            {
+                Console.Write("\nОШИБКА!!! Попробуйте ещё раз . . . ");
+                moveX();
+            }
+        }
+        private void moveY()
+        {
+            string input;
+            int move = 0;
+            Console.WriteLine("\n(Положительное число ― вверх,  отрицательное - вниз)\nВведите, какое расстояние хотите пройти: ");
+            input = Console.ReadLine();
+            if (Int32.TryParse(input, out move) == true)
+            {
+                if ((y += move) <= 10 || (y += move) >= 0)
+                {
+                    y += move;
+                    Console.WriteLine($"\nГотово! Ваши текущие координаты: {x}; {y}");
+                }
+                else
+                {
+                    Console.Write("\nОШИБКА!!! Нельзя выходить за пределы карты\nПопробуйте ещё раз . . . ");
+                    moveX();
+                }
+            }
+            else
+            {
+                Console.Write("\nОШИБКА!!! Попробуйте ещё раз . . . ");
+                moveX();
+            }
+        }
+        private void Heal()
         {
             hp += 4;
-            if (hp < 20)
+            if (hp < hpMax)
                 Console.WriteLine("Вы вылечились на 4 HP");
             else
             {
                 Console.WriteLine("Вы полностью вылечились!");
-                hp = 20;
+                hp = hpMax;
             }
         }
         public void FullHeal()
         {
-            hp = 20;
+            hp = hpMax;
             Console.WriteLine("Вы полностью вылечились!");
         }
         public void ChangeLager()
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,16 +12,8 @@ namespace character
         static void Main(string[] args)
         {
             Console.Title = "Игровой персонаж";
-            int xMax = 5;
-            int yMax = 5;
-            Player[] players = new Player[10];
-            for (int i = 0; i < players.Length; i++)
-            {
-                players[i] = new Player();
-            }
-            players[0].Info(xMax, yMax);
-            Console.WriteLine();
-            players[0].Output();
+            List<Player> players = new List<Player>();
+            players[0].Start();
         }
     }
 }
